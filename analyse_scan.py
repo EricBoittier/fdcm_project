24fe1c13625cd77a6478278a41051042325b3c5a
EricBoittier <eric.boittier@icloud.com>
2021-11-24 17:15:25 +0100

    added scan energies

--- analyse_scan.py
+++ analyse_scan.py
@@ -176,6 +176,7 @@ def analyse(args):
 
     lc_df = pd.DataFrame(local_charges)
     print(lc_df)
+    # adding energies
     print(frames, len(a1_), len(a2_), len(d1_), len(frames))
     df = pd.DataFrame({"frame": frames, "energy": scan_energies, "local_file" : local_file_names, "error": errors, "a1": a1_, "a2": a2_, "d1": d1_})
     df = df.join(lc_df)
