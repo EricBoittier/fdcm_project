5f5787161602525ad82c479fe05f94d14c4b6917
EricBoittier <eric.boittier@icloud.com>
2021-12-01 14:59:47 +0100

    graph traversal scan, launched from args file and job_maker

--- job_maker.py
+++ job_maker.py
@@ -112,7 +112,7 @@ def template_neighbours_from_2drmsd(args, do_neighbours=True):
         if do_neighbours:
             for n in neighbours:
                 tmp_str = template_fit(args, start, n, first=is_first, prev_frame=previous)
-                _fpath = os.path.join(args.job_folder, f"frame_{start}_{n}.sh")
+                _fpath = os.path.join(args.job_folder, f"frame_{start}_{n[1]}.sh")
                 f_ = open(_fpath, "w")
                 f_.write(tmp_str)
                 f_.close()
