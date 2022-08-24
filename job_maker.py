389b221bdb2f7e6889a37f2f787a1eaad754170a
EricBoittier <eric.boittier@icloud.com>
2022-08-24 15:41:37 +0200

    read G and anytree from pickle

--- job_maker.py
+++ job_maker.py
@@ -1,10 +1,9 @@
 import os
-
+import pandas as pd
 import jinja2
 from jinja2 import StrictUndefined
 
-#from analyse_scan import get_path_neighbours_from_gaussian_scan
-from create_neighbours import schedule_from_2drmsd
+from create_neighbours import schedule_from_2drmsd, scan_neighbours_schedule_from_anytree_graph
 
 """
 Templates
@@ -68,36 +67,54 @@ def template_fit(args, start_frame, next_frame, prev_frame=None, first=False):
     return output
 
 
-# def template_neighbours_from_gaussian_scan(args, do_neighbours=True):
-#     paths, neighbours = get_path_neighbours_from_gaussian_scan(args)
-#     if not os.path.exists(args.job_folder):
-#         os.makedirs(args.job_folder)
-#     n_jobs = len(paths)
-#     for i, (path, neighbour) in enumerate(zip(paths, neighbours)):
-#         if i < n_jobs - 1:
-#             is_first = (i == 0)
-#             print(i, path, neighbour)
-#             tmp_str = template_fit(args, paths[i], paths[i + 1], first=is_first, prev_frame=paths[i - 1])
-#             f = open(os.path.join(args.job_folder, f"frame_{paths[i]}_{paths[i + 1]}.sh"), "w")
-#             f.write(tmp_str)
-#             if do_neighbours:
-#                 for n in neighbour:
-#                     tmp_str = template_fit(args, paths[i], n, first=is_first, prev_frame=paths[i - 1])
-#                     _fpath = os.path.join(args.job_folder, f"frame_{paths[i]}_{n}.sh")
-#                     f_ = open(_fpath, "w")
-#                     f_.write(tmp_str)
-#                     f_.close()
-#                     f.write(f"\nsbatch {_fpath} \n")
-#             try:
-#                 next_job = os.path.join(args.job_folder, f"frame_{paths[i + 1]}_{paths[i + 2]}.sh")
-#                 f.write(f"\nsbatch {next_job} \n")
-#             except IndexError:
-#                 pass
-#             f.close()
-
-
 def template_neighbours_from_2drmsd(args, do_neighbours=True):
     scan_neighbours_schedule = schedule_from_2drmsd(args.rmsd)
+    #  Make directory if needed
+    if not os.path.exists(args.job_folder):
+        os.makedirs(args.job_folder)
+
+    #  list to keep track of jobs so a file isn't overwritten.
+    jobs_submitted = []
+    #  Process the schedule for job writing
+    for i, (scan, neighbours, schedule) in enumerate(scan_neighbours_schedule):
+        print(i, scan, neighbours, schedule)
+
+        previous, start, end = scan
+        is_first = (0 == previous)
+        tmp_str = template_fit(args, start, end, first=is_first, prev_frame=previous)
+        with open(os.path.join(args.job_folder, f"frame_{start}_{end}.sh"), "w") as f:
+            jobs_submitted.append(os.path.join(args.job_folder, f"frame_{start}_{end}.sh"))
+            print(os.path.join(args.job_folder, f"frame_{start}_{end}.sh"))
+            f.write(tmp_str)
+
+            #  write the neighbour jobs and schedule them
+            if do_neighbours:
+                for n in neighbours:
+                    _fpath = os.path.join(args.job_folder, f"frame_{start}_{n[1]}.sh")
+                    if _fpath not in jobs_submitted:
+                        tmp_str = template_fit(args, start, n[1], first=is_first, prev_frame=previous)
+
+                        f_ = open(_fpath, "w")
+                        f_.write(tmp_str)
+
+                        print(f"\nsbatch {_fpath} \n")
+                        f.write(f"\nsbatch {_fpath} \n")
+
+            #  schedule the jobs preceding this one (i.e. all branches from this node)
+            for job in schedule:
+                next_job = os.path.join(args.job_folder, f"frame_{job[0]}_{job[1]}.sh")
+                f.write(f"\nsbatch {next_job} \n")
+                print(next_job)
+
+
+def template_neighbours_from_anytree_and_G(args, do_neighbours=True):
+    # scan_neighbours_schedule = schedule_from_2drmsd(args.rmsd)
+
+    anytree = pd.read_pickle(args.anytree)
+    G = pd.read_pickle(args.G)
+
+    scan_neighbours_schedule = scan_neighbours_schedule_from_anytree_graph(anytree, G)
+
     #  Make directory if needed
     if not os.path.exists(args.job_folder):
         os.makedirs(args.job_folder)
