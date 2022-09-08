8b6720422204ca85a98a7ea830b109fd8afd7ce3
EricBoittier <eric.boittier@icloud.com>
2022-09-08 12:44:06 +0200

    making m_strings

--- src/m_strings.f90
+++ src/m_strings.f90
@@ -49,7 +49,7 @@ PUBLIC len_white       !  find location of last non-whitespace character
 PUBLIC noesc           !  elemental function converts non-printable ASCII8 characters to a space
 PUBLIC notabs          !  convert tabs to spaces in output while maintaining columns, assuming a tab is set every 8 characters
 PUBLIC expand          !  expand escape sequences in a string
-PUBLIC visible         !  expand escape sequences in a string to control and meta-control representations
+!PUBLIC visible         !  expand escape sequences in a string to control and meta-control representations
 !----------------------# NUMERIC STRINGS
 PUBLIC string_to_value !  generic subroutine returns REAL|DOUBLEPRECISION|INTEGER value from string (a2d,a2r,a2i)
  PRIVATE a2d           !  subroutine returns double value from string
@@ -2656,53 +2656,53 @@ end function indent
 !!     The expansion is not reversible, as input sequences such as "M-" or "^a"
 !!     will look like expanded sequences.
 !===================================================================================================================================
-function visible(input) result(output)
-character(len=*),intent(in)  :: input
-character(len=:),allocatable :: output
-
-character(len=*),parameter::ident_25="&
-&@(#)M_strings::visible(3f) expand escape sequences in a string to control and meta-control representations"
-
-integer                      :: i
-character(len=1)             :: c
-
-character(len=*),parameter :: chars(0:255)= [ &
-'^@  ', '^A  ', '^B  ', '^C  ', '^D  ', '^E  ', '^F  ', '^G  ', '^H  ', '^I  ', &
-'^J  ', '^K  ', '^L  ', '^M  ', '^N  ', '^O  ', '^P  ', '^Q  ', '^R  ', '^S  ', &
-'^T  ', '^U  ', '^V  ', '^W  ', '^X  ', '^Y  ', '^Z  ', '^[  ', '^\\  ', '^]  ', &
-'^^  ', '^_  ', '    ', '!   ', '"   ', '#   ', '$   ', '%   ', '&   ', '''   ', &
-'(   ', ')   ', '*   ', '+   ', ',   ', '-   ', '.   ', '/   ', '0   ', '1   ', &
-'2   ', '3   ', '4   ', '5   ', '6   ', '7   ', '8   ', '9   ', ':   ', ';   ', &
-'<   ', '=   ', '>   ', '?   ', '@   ', 'A   ', 'B   ', 'C   ', 'D   ', 'E   ', &
-'F   ', 'G   ', 'H   ', 'I   ', 'J   ', 'K   ', 'L   ', 'M   ', 'N   ', 'O   ', &
-'P   ', 'Q   ', 'R   ', 'S   ', 'T   ', 'U   ', 'V   ', 'W   ', 'X   ', 'Y   ', &
-'Z   ', '[   ', '\\   ', ']   ', '^   ', '_   ', '`   ', 'a   ', 'b   ', 'c   ', &
-'d   ', 'e   ', 'f   ', 'g   ', 'h   ', 'i   ', 'j   ', 'k   ', 'l   ', 'm   ', &
-'n   ', 'o   ', 'p   ', 'q   ', 'r   ', 's   ', 't   ', 'u   ', 'v   ', 'w   ', &
-'x   ', 'y   ', 'z   ', '{   ', '|   ', '}   ', '~   ', '^?  ', 'M-^@', 'M-^A', &
-'M-^B', 'M-^C', 'M-^D', 'M-^E', 'M-^F', 'M-^G', 'M-^H', 'M-^I', 'M-^J', 'M-^K', &
-'M-^L', 'M-^M', 'M-^N', 'M-^O', 'M-^P', 'M-^Q', 'M-^R', 'M-^S', 'M-^T', 'M-^U', &
-'M-^V', 'M-^W', 'M-^X', 'M-^Y', 'M-^Z', 'M-^[', 'M-^\\', 'M-^]', 'M-^^', 'M-^_', &
-'M-  ', 'M-! ', 'M-" ', 'M-# ', 'M-$ ', 'M-% ', 'M-& ', 'M-'' ', 'M-( ', 'M-) ', &
-'M-* ', 'M-+ ', 'M-, ', 'M-- ', 'M-. ', 'M-/ ', 'M-0 ', 'M-1 ', 'M-2 ', 'M-3 ', &
-'M-4 ', 'M-5 ', 'M-6 ', 'M-7 ', 'M-8 ', 'M-9 ', 'M-: ', 'M-; ', 'M-< ', 'M-= ', &
-'M-> ', 'M-? ', 'M-@ ', 'M-A ', 'M-B ', 'M-C ', 'M-D ', 'M-E ', 'M-F ', 'M-G ', &
-'M-H ', 'M-I ', 'M-J ', 'M-K ', 'M-L ', 'M-M ', 'M-N ', 'M-O ', 'M-P ', 'M-Q ', &
-'M-R ', 'M-S ', 'M-T ', 'M-U ', 'M-V ', 'M-W ', 'M-X ', 'M-Y ', 'M-Z ', 'M-[ ', &
-'M-\\ ', 'M-] ', 'M-^ ', 'M-_ ', 'M-` ', 'M-a ', 'M-b ', 'M-c ', 'M-d ', 'M-e ', &
-'M-f ', 'M-g ', 'M-h ', 'M-i ', 'M-j ', 'M-k ', 'M-l ', 'M-m ', 'M-n ', 'M-o ', &
-'M-p ', 'M-q ', 'M-r ', 'M-s ', 'M-t ', 'M-u ', 'M-v ', 'M-w ', 'M-x ', 'M-y ', &
-'M-z ', 'M-{ ', 'M-| ', 'M-} ', 'M-~ ', 'M-^?']
-output=''
-do i=1,len(input)
-   c=input(i:i)
-   if(c.eq.' ')then
-      output=output//' '
-   else
-      output=output//trim(chars(ichar(c)))
-   endif
-enddo
-end function visible
+!function visible(input) result(output)
+!character(len=*),intent(in)  :: input
+!character(len=:),allocatable :: output
+!
+!character(len=*),parameter::ident_25="&
+!&@(#)M_strings::visible(3f) expand escape sequences in a string to control and meta-control representations"
+!
+!integer                      :: i
+!character(len=1)             :: c
+!
+!character(len=*),parameter :: chars(0:255)= [ &
+!'^@  ', '^A  ', '^B  ', '^C  ', '^D  ', '^E  ', '^F  ', '^G  ', '^H  ', '^I  ', &
+!'^J  ', '^K  ', '^L  ', '^M  ', '^N  ', '^O  ', '^P  ', '^Q  ', '^R  ', '^S  ', &
+!'^T  ', '^U  ', '^V  ', '^W  ', '^X  ', '^Y  ', '^Z  ', '^[  ', '^\\  ', '^]  ', &
+!'^^  ', '^_  ', '    ', '!   ', '"   ', '#   ', '$   ', '%   ', '&   ', '''   ', &
+!'(   ', ')   ', '*   ', '+   ', ',   ', '-   ', '.   ', '/   ', '0   ', '1   ', &
+!'2   ', '3   ', '4   ', '5   ', '6   ', '7   ', '8   ', '9   ', ':   ', ';   ', &
+!'<   ', '=   ', '>   ', '?   ', '@   ', 'A   ', 'B   ', 'C   ', 'D   ', 'E   ', &
+!'F   ', 'G   ', 'H   ', 'I   ', 'J   ', 'K   ', 'L   ', 'M   ', 'N   ', 'O   ', &
+!'P   ', 'Q   ', 'R   ', 'S   ', 'T   ', 'U   ', 'V   ', 'W   ', 'X   ', 'Y   ', &
+!'Z   ', '[   ', '\\   ', ']   ', '^   ', '_   ', '`   ', 'a   ', 'b   ', 'c   ', &
+!'d   ', 'e   ', 'f   ', 'g   ', 'h   ', 'i   ', 'j   ', 'k   ', 'l   ', 'm   ', &
+!'n   ', 'o   ', 'p   ', 'q   ', 'r   ', 's   ', 't   ', 'u   ', 'v   ', 'w   ', &
+!'x   ', 'y   ', 'z   ', '{   ', '|   ', '}   ', '~   ', '^?  ', 'M-^@', 'M-^A', &
+!'M-^B', 'M-^C', 'M-^D', 'M-^E', 'M-^F', 'M-^G', 'M-^H', 'M-^I', 'M-^J', 'M-^K', &
+!'M-^L', 'M-^M', 'M-^N', 'M-^O', 'M-^P', 'M-^Q', 'M-^R', 'M-^S', 'M-^T', 'M-^U', &
+!'M-^V', 'M-^W', 'M-^X', 'M-^Y', 'M-^Z', 'M-^[', 'M-^\\', 'M-^]', 'M-^^', 'M-^_', &
+!'M-  ', 'M-! ', 'M-" ', 'M-# ', 'M-$ ', 'M-% ', 'M-& ', 'M-'' ', 'M-( ', 'M-) ', &
+!'M-* ', 'M-+ ', 'M-, ', 'M-- ', 'M-. ', 'M-/ ', 'M-0 ', 'M-1 ', 'M-2 ', 'M-3 ', &
+!'M-4 ', 'M-5 ', 'M-6 ', 'M-7 ', 'M-8 ', 'M-9 ', 'M-: ', 'M-; ', 'M-< ', 'M-= ', &
+!'M-> ', 'M-? ', 'M-@ ', 'M-A ', 'M-B ', 'M-C ', 'M-D ', 'M-E ', 'M-F ', 'M-G ', &
+!'M-H ', 'M-I ', 'M-J ', 'M-K ', 'M-L ', 'M-M ', 'M-N ', 'M-O ', 'M-P ', 'M-Q ', &
+!'M-R ', 'M-S ', 'M-T ', 'M-U ', 'M-V ', 'M-W ', 'M-X ', 'M-Y ', 'M-Z ', 'M-[ ', &
+!'M-\\ ', 'M-] ', 'M-^ ', 'M-_ ', 'M-` ', 'M-a ', 'M-b ', 'M-c ', 'M-d ', 'M-e ', &
+!'M-f ', 'M-g ', 'M-h ', 'M-i ', 'M-j ', 'M-k ', 'M-l ', 'M-m ', 'M-n ', 'M-o ', &
+!'M-p ', 'M-q ', 'M-r ', 'M-s ', 'M-t ', 'M-u ', 'M-v ', 'M-w ', 'M-x ', 'M-y ', &
+!'M-z ', 'M-{ ', 'M-| ', 'M-} ', 'M-~ ', 'M-^?']
+!output=''
+!do i=1,len(input)
+!   c=input(i:i)
+!   if(c.eq.' ')then
+!      output=output//' '
+!   else
+!      output=output//trim(chars(ichar(c)))
+!   endif
+!enddo
+!end function visible
 !===================================================================================================================================
 !()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()!
 !===================================================================================================================================
@@ -2730,7 +2730,6 @@ end function visible
 !!       \\e     escape
 !!       \\f     form feed
 !!       \
-     new line
 !!       \\r     carriage return
 !!       \\t     horizontal tab
 !!       \\v     vertical tab
@@ -2765,9 +2764,6 @@ end function visible
 !!      \\tABC\\tabc
 !!      \\tA\\a
 !!      \
-ONE\
-TWO\
-THREE
 !===================================================================================================================================
 function expand(line,escape) result(lineout)
 USE ISO_C_BINDING ,ONLY: c_horizontal_tab
